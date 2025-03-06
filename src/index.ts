d5ee4bd40d2776b2942a112218c6128329b9fd4e
Tristan Davey <tristan@tristandavey.com>
2025-03-06 22:20:16 +1000

    Refactor to break down oversized functions

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import type { Polygon, Feature, FeatureCollection, Position, LineString } from '
 
 interface mapFitOptions {
   tileSize?: number;
-  antimeridian?: boolean;
+  preferredBearing?: number;
+  floatZoom?: boolean;
 }
 
 interface mapFitPadding {
@@ -28,13 +29,145 @@ interface rectangleOrientation {
 
 interface boundingOrientation {
   bearing: number | undefined;
-  shortSide: Feature<LineString> | undefined;
-  longSide: Feature<LineString> | undefined;
+  orientation: rectangleOrientation;
   envelope: Feature<Polygon> | undefined;
 }
 
 type XY = [number, number];
 type LonLat = [number, number];
+
+function mapFitFeatures({
+  features,
+  screenDimensions,
+  mapPadding = {} as mapFitPadding,
+  maxZoom = 20,
+  options = {} as mapFitOptions,
+}: {
+  features: FeatureCollection;
+  screenDimensions: XY;
+  mapPadding?: mapFitPadding;
+  maxZoom?: number;
+  options?: mapFitOptions;
+}): mapFitResult {
+  // Set default options
+  const { tileSize = 512, preferredBearing = 0 } = options;
+
+  // Create a mercator projection. SphericalMercator caches its calculations so it's safe to create a new instance each run
+  const merc: SphericalMercator = new SphericalMercator({ size: tileSize, antimeridian: true });
+  const [screenWidth, screenHeight] = screenDimensions;
+  const { left = 0, right = 0, top = 0, bottom = 0 } = mapPadding;
+  const paddedScreenWidth = screenWidth - left - right;
+  const paddedScreenHeight = screenHeight - top - bottom;
+  const paddedScreenRatio = paddedScreenWidth / paddedScreenHeight;
+
+  // Calculate the bounding rectangle of the features
+  const {
+    boundsOrientation: { orientation, bearing: baseBearing },
+    boundingRectangle,
+  } = minimumBoundingRectangle(features);
+
+  if (!boundingRectangle) {
+    throw new Error('Unable to calculate bounding rectangle');
+  }
+
+  // Determine how to fit the bounding rectangle to the screen
+  const zoom = findScreenZoom([paddedScreenWidth, paddedScreenHeight], paddedScreenRatio, orientation, maxZoom, merc);
+  const bearing = findScreenBearing(baseBearing!, preferredBearing, paddedScreenRatio);
+  const center = findScreenCenter(boundingRectangle, bearing, zoom, mapPadding, merc);
+
+  return { bearing, zoom, center };
+}
+
+function findScreenZoom(
+  paddedScreenDimensions: XY,
+  paddedScreenRatio: number,
+  boundingRectangleOrientation: rectangleOrientation,
+  maxZoom: number,
+  merc: SphericalMercator,
+): number {
+  const { shortSide, longSide } = boundingRectangleOrientation;
+  const longSideCoords = turf.getCoords(longSide!);
+  const shortSideCoords = turf.getCoords(shortSide!);
+
+  // We need to determine the ratio required for the zoom level. To do this we are going to approximate the length
+  // of the longest and shortest sides of the polygon in pixels (This doesn't account for projection distortion but is
+  // a good estimation)
+  const longPx: [XY, XY] = [merc.px(longSideCoords[0], maxZoom), merc.px(longSideCoords[1], maxZoom)];
+  const shortPx: [XY, XY] = [merc.px(shortSideCoords[0], maxZoom), merc.px(shortSideCoords[1], maxZoom)];
+
+  // Because these points aren't aligned to the axis, we use the Pythagorean theorem to calculate the distance
+  const longPxX = longPx[0][0] - longPx[1][0];
+  const longPxY = longPx[0][1] - longPx[1][1];
+  const shortPxX = shortPx[0][0] - shortPx[1][0];
+  const shortPxY = shortPx[0][1] - shortPx[1][1];
+  const longPxDistance = Math.sqrt(Math.pow(longPxX, 2) + Math.pow(longPxY, 2));
+  const shortPxDistance = Math.sqrt(Math.pow(shortPxX, 2) + Math.pow(shortPxY, 2));
+
+  let xPx = longPxDistance;
+  let yPx = shortPxDistance;
+
+  // If the screen is taller than it is wide, swap the x and y values
+  if (paddedScreenRatio < 1) {
+    xPx = shortPxDistance;
+    yPx = longPxDistance;
+  }
+
+  const ratios: XY = [Math.abs(xPx / paddedScreenDimensions[0]), Math.abs(yPx / paddedScreenDimensions[1])];
+  return Math.min(maxZoom - Math.log(ratios[0]) / Math.log(2), maxZoom - Math.log(ratios[1]) / Math.log(2));
+}
+
+function findScreenBearing(boundingRectangleBearing: number, preferredBearing: number, screenRatio: number): number {
+  let bearing = boundingRectangleBearing;
+  // Rotate the bearing by 90 degrees if the screen is wider than it is tall
+  if (screenRatio > 1) {
+    bearing = bearing + 90;
+  }
+
+  // Rotate the bearing 180 degrees if the preferred bearing is on the opposite side of the screen
+  if (Math.abs(bearing - preferredBearing) > 180) {
+    bearing = bearing + 180;
+  }
+
+  // Ensure the bearing is between 0 and 360
+  bearing = bearing % 360;
+
+  return bearing;
+}
+
+function findScreenCenter(
+  boundingRectangle: Feature<Polygon>,
+  bearing: number,
+  zoom: number,
+  padding: mapFitPadding,
+  merc: SphericalMercator,
+) {
+  const { left = 0, right = 0, top = 0, bottom = 0 } = padding;
+  const centroid = turf.centroid(boundingRectangle);
+  if (!centroid) {
+    throw new Error('Unable to calculate centre of surrounding rectangle');
+  }
+  const center = turf.getCoord(centroid)!;
+  if (!isLonLat(center)) {
+    throw new Error('Unable to calculate centre of surrounding rectangle');
+  }
+
+  const centerPx = merc.px(center, zoom);
+
+  // Calculate the offset required to center the polygon in the viewport
+  const xPaddingOffset = right - left;
+  const yPaddingOffset = bottom - top;
+
+  if (xPaddingOffset != 0 || yPaddingOffset != 0) {
+    const bearingRadians = bearing * (Math.PI / 180);
+
+    const centerXOffset = xPaddingOffset * Math.cos(bearingRadians) - yPaddingOffset * Math.sin(bearingRadians);
+    const centerYOffset = xPaddingOffset * Math.sin(bearingRadians) + yPaddingOffset * Math.cos(bearingRadians);
+
+    return merc.ll([centerPx[0] + centerXOffset, centerPx[1] + centerYOffset], zoom);
+  } else {
+    return merc.ll(centerPx, zoom);
+  }
+}
 
 export function minimumBoundingRectangle(geoJsonInput: turf.AllGeoJSON): {
   boundsOrientation: boundingOrientation;
@@ -51,15 +184,16 @@ export function minimumBoundingRectangle(geoJsonInput: turf.AllGeoJSON): {
     (smallestEnvelope: boundingOrientation | undefined, segment) => {
       return smallestHullEnvelopeReducer(smallestEnvelope, segment!, convexHull);
     },
-    { bearing: undefined, shortSide: undefined, longSide: undefined, envelope: undefined },
+    { bearing: undefined, orientation: { shortSide: undefined, longSide: undefined }, envelope: undefined },
   );
 
   const boundingRectangle = turf.transformRotate(
     turf.envelope(smallestHullBoundsOrientation.envelope!),
     smallestHullBoundsOrientation.bearing!,
     {
-    pivot: turf.centroid(convexHull),
-  });
+      pivot: turf.centroid(convexHull),
+    },
+  );
 
   return {
     boundsOrientation: smallestHullBoundsOrientation,
@@ -80,118 +214,17 @@ function smallestHullEnvelopeReducer(
   });
   const envelopeOfHull = turf.envelope(rotatedHull);
 
-  const { shortSide, longSide } = findRectangleOrientation(envelopeOfHull)!;
-  const shortSideLength = turf.length(shortSide!);
+  const rectangleOrientation = findRectangleOrientation(envelopeOfHull);
+  const shortSideLength = turf.length(rectangleOrientation.shortSide!);
 
-  if (smallestEnvelope!.shortSide == undefined || shortSideLength < turf.length(smallestEnvelope!.shortSide)) {
-    return { bearing, shortSide: shortSide!, longSide: longSide!, envelope: envelopeOfHull };
+  if (
+    smallestEnvelope!.orientation.shortSide == undefined ||
+    shortSideLength < turf.length(smallestEnvelope!.orientation.shortSide)
+  ) {
+    return { bearing, orientation: rectangleOrientation, envelope: envelopeOfHull };
   }
 
   return smallestEnvelope!;
-}
-
-function mapFitFeatures({
-  features,
-  screenDimensions,
-  mapPadding = {} as mapFitPadding,
-  maxZoom = 20,
-  options = {} as mapFitOptions,
-}: {
-  features: FeatureCollection;
-  screenDimensions: XY;
-  mapPadding?: mapFitPadding;
-  maxZoom?: number;
-  options?: mapFitOptions;
-}): mapFitResult {
-  const { tileSize = 512, antimeridian = true } = options;
-
-  const merc: SphericalMercator = new SphericalMercator({
-    size: tileSize,
-    antimeridian: antimeridian,
-  });
-  const {
-    boundsOrientation: { shortSide, longSide, bearing: baseBearing },
-    boundingRectangle,
-  } = minimumBoundingRectangle(features);
-
-  if (!boundingRectangle) {
-    throw new Error('Unable to calculate bounding rectangle');
-  }
-
-  const longSideCoords = turf.getCoords(longSide!);
-  const shortSideCoords = turf.getCoords(shortSide!);
-
-  // We need to determine the ratio required for the zoom level. To do this we are going to approximate the length
-  // of the longest and shortest sides of the polygon in pixels (This doesn't account for projection distortion but is
-  // a good estimation)
-  const longPx: [XY, XY] = [merc.px(longSideCoords[0], maxZoom), merc.px(longSideCoords[1], maxZoom)];
-  const shortPx: [XY, XY] = [merc.px(shortSideCoords[0], maxZoom), merc.px(shortSideCoords[1], maxZoom)];
-
-  // Because these points aren't aligned to the axis, we use the Pythagorean theorem to calculate the distance
-  const longPxX = longPx[0][0] - longPx[1][0];
-  const longPxY = longPx[0][1] - longPx[1][1];
-  const shortPxX = shortPx[0][0] - shortPx[1][0];
-  const shortPxY = shortPx[0][1] - shortPx[1][1];
-  const longPxDistance = Math.sqrt(Math.pow(longPxX, 2) + Math.pow(longPxY, 2));
-  const shortPxDistance = Math.sqrt(Math.pow(shortPxX, 2) + Math.pow(shortPxY, 2));
-
-  let xPx = 0;
-  let yPx = 0;
-
-  // Use screenDimensions to calculate widest screen dimension, use widest polygon edge to calculate base bearing
-  const [screenWidth, screenHeight] = screenDimensions;
-  const { left = 0, right = 0, top = 0, bottom = 0 } = mapPadding;
-  const xPadding = left + right;
-  const yPadding = top + bottom;
-
-  // If the screen is wider than it is tall, rotate the bearing by 90 degrees
-  let bearing = 0;
-  if (screenWidth + xPadding > screenHeight + yPadding) {
-    bearing = baseBearing! + 90;
-    xPx = longPxDistance;
-    yPx = shortPxDistance;
-  } else {
-    bearing = baseBearing!;
-    xPx = shortPxDistance;
-    yPx = longPxDistance;
-  }
-
-  const ratios: XY = [Math.abs(xPx / (screenWidth - xPadding)), Math.abs(yPx / (screenHeight - yPadding))];
-  const zoom = getOptimalZoom(maxZoom, ratios);
-
-  const centroid = turf.centroid(boundingRectangle);
-  if (!centroid) {
-    throw new Error('Unable to calculate centre of surrounding rectangle');
-  }
-  const center = turf.getCoord(centroid)!;
-  if (!isLonLat(center)) {
-    throw new Error('Unable to calculate centre of surrounding rectangle');
-  }
-
-  const centerPx = merc.px(center, zoom);
-
-  if (bearing < 0) {
-    bearing = 360 + bearing;
-  }
-
-  // Calculate the offset required to center the polygon in the viewport
-  const xPaddingOffset = right - left;
-  const yPaddingOffset = bottom - top;
-
-  if (xPaddingOffset != 0 || yPaddingOffset != 0) {
-    const bearingRadians = bearing * (Math.PI / 180);
-
-    const centerXOffset = xPaddingOffset * Math.cos(bearingRadians) - yPaddingOffset * Math.sin(bearingRadians);
-    const centerYOffset = xPaddingOffset * Math.sin(bearingRadians) + yPaddingOffset * Math.cos(bearingRadians);
-
-    return {
-      bearing,
-      zoom,
-      center: merc.ll([centerPx[0] + centerXOffset, centerPx[1] + centerYOffset], zoom),
-    };
-  } else {
-    return { bearing, zoom, center: merc.ll(centerPx, zoom) };
-  }
 }
 
 function findRectangleOrientation(rectangle: Feature<Polygon>): rectangleOrientation {
@@ -217,10 +250,6 @@ function findRectangleOrientation(rectangle: Feature<Polygon>): rectangleOrienta
 
 function isLonLat(lonLat: Position): lonLat is LonLat {
   return lonLat.length === 2 && lonLat.every((coord) => typeof coord === 'number');
-}
-
-function getOptimalZoom(baseZoom: number, ratios: [number, number]) {
-  return Math.min(baseZoom - Math.log(ratios[0]) / Math.log(2), baseZoom - Math.log(ratios[1]) / Math.log(2));
 }
 
 export { mapFitFeatures };
